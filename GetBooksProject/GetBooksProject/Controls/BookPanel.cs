851521288cdf6533ad8dd6d975778b8f4a32a54e
vladZelenkov <mozzz475@gmail.com>
2023-09-29 19:01:05 +0300

    change tabControl and create BookPanel for show books on tabControl

---
+++ GetBooksProject/GetBooksProject/Controls/BookPanel.cs
@@ -0,0 +1,66 @@
+﻿using GetBooksProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetBooksProject.Controls
+{
+    class BookPanel : Panel
+    {
+        private Book _book;
+        private Label _name;
+        private Label _author;
+        private Label _price;
+
+        public BookPanel(Book book, bool isPriceShow)
+        {
+            _book = book;
+            _name = new Label();
+            _author = new Label();
+            _price = new Label();
+            IsPriceShow = isPriceShow;
+            SetParameters();
+        }
+
+        public bool IsPriceShow { get; set; }
+
+        private void SetParameters()
+        {
+            Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            BorderStyle = BorderStyle.FixedSingle;
+            _name.Name = "nameLabel";
+            _author.Name = "authorLabel";
+            _price.Name = "priceLabel";
+            _name.Text = _book.Name;
+            _author.Text = _book.Author;
+            //добавить Price в класс Book
+            _price.Visible = false;
+            AddLabel(_name);
+            AddLabel(_author);
+            AddLabel(_price);
+        }
+
+        private void AddLabel(Label label)
+        {
+            int indent = 5;
+            System.Drawing.Size minSize = new System.Drawing.Size();
+            minSize.Width = Width;
+
+            label.AutoSize = true;
+            label.MinimumSize = minSize;
+            label.Margin = new Padding(0, indent, 0, indent);
+            int top = 0;
+
+            foreach (Control control in Controls)
+            {
+                top += control.Height + indent;
+            }
+
+            label.Top = top;
+            Controls.Add(label);
+        }
+    }
+}
--- GetBooksProject/GetBooksProject/Form1.Designer.cs
+++ GetBooksProject/GetBooksProject/Form1.Designer.cs
@@ -30,15 +30,19 @@ namespace GetBooksProject
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.myBooksTabPage = new System.Windows.Forms.TabPage();
+            this.findBooksTabPage = new System.Windows.Forms.TabPage();
+            this.myBooksFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.findBooksFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.tabControl1.SuspendLayout();
+            this.myBooksTabPage.SuspendLayout();
+            this.findBooksTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.myBooksTabPage);
+            this.tabControl1.Controls.Add(this.findBooksTabPage);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
@@ -46,25 +50,43 @@ namespace GetBooksProject
             this.tabControl1.Size = new System.Drawing.Size(896, 462);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // myBooksTabPage
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(888, 436);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Мои книги";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.myBooksTabPage.Controls.Add(this.myBooksFlowLayoutPanel);
+            this.myBooksTabPage.Location = new System.Drawing.Point(4, 22);
+            this.myBooksTabPage.Name = "myBooksTabPage";
+            this.myBooksTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.myBooksTabPage.Size = new System.Drawing.Size(888, 436);
+            this.myBooksTabPage.TabIndex = 0;
+            this.myBooksTabPage.Text = "Мои книги";
+            this.myBooksTabPage.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // findBooksTabPage
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(888, 436);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Найти книги";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.findBooksTabPage.Controls.Add(this.findBooksFlowLayoutPanel);
+            this.findBooksTabPage.Location = new System.Drawing.Point(4, 22);
+            this.findBooksTabPage.Name = "findBooksTabPage";
+            this.findBooksTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.findBooksTabPage.Size = new System.Drawing.Size(888, 436);
+            this.findBooksTabPage.TabIndex = 1;
+            this.findBooksTabPage.Text = "Найти книги";
+            this.findBooksTabPage.UseVisualStyleBackColor = true;
+            // 
+            // myBooksFlowLayoutPanel
+            // 
+            this.myBooksFlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myBooksFlowLayoutPanel.Location = new System.Drawing.Point(3, 3);
+            this.myBooksFlowLayoutPanel.Name = "myBooksFlowLayoutPanel";
+            this.myBooksFlowLayoutPanel.Size = new System.Drawing.Size(882, 430);
+            this.myBooksFlowLayoutPanel.TabIndex = 0;
+            // 
+            // findBooksFlowLayoutPanel
+            // 
+            this.findBooksFlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.findBooksFlowLayoutPanel.Location = new System.Drawing.Point(3, 3);
+            this.findBooksFlowLayoutPanel.Name = "findBooksFlowLayoutPanel";
+            this.findBooksFlowLayoutPanel.Size = new System.Drawing.Size(882, 430);
+            this.findBooksFlowLayoutPanel.TabIndex = 0;
             // 
             // Form1
             // 
@@ -75,6 +97,8 @@ namespace GetBooksProject
             this.Name = "Form1";
             this.Text = "GetBooks";
             this.tabControl1.ResumeLayout(false);
+            this.myBooksTabPage.ResumeLayout(false);
+            this.findBooksTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -82,8 +106,10 @@ namespace GetBooksProject
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage myBooksTabPage;
+        private System.Windows.Forms.TabPage findBooksTabPage;
+        private System.Windows.Forms.FlowLayoutPanel myBooksFlowLayoutPanel;
+        private System.Windows.Forms.FlowLayoutPanel findBooksFlowLayoutPanel;
     }
 }
 
