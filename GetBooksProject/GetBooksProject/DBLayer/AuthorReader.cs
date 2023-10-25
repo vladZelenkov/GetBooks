4e06e2a4f14a36295680ee610497ddf6aba2c62e
vladZelenkov <mozzz475@gmail.com>
2023-10-25 19:01:02 +0300

    added class AuthorReader for getting authors from db

---
+++ GetBooksProject/GetBooksProject/DBLayer/AuthorReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace GetBooksProject.DBLayer
+{
+    class AuthorReader : DBReader
+    {
+        public List<string> GetAllAuthors()
+        {
+            List<string> authors = (List<string>)GetForCommand("select name from authors");
+            return authors;
+        }
+
+        protected override object GetResult(SQLiteCommand command)
+        {
+            List<string> authors = new List<string>();
+
+            using (SQLiteDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        string author = reader.GetString(0);
+                        authors.Add(author);
+                    }
+                }
+            }
+
+            return authors;
+        }
+    }
+}
