cca15c7613dac07596b5f0a017b0846ccb6ef586
vladZelenkov <mozzz475@gmail.com>
2023-10-30 15:17:58 +0300

    implemented button find for Labirint

--- GetBooksProject/GetBooksProject/Controls/ProductFindGroup.cs
+++ GetBooksProject/GetBooksProject/Controls/ProductFindGroup.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GetBooksProject.Entity;
+using GetBooksProject.URLLayer;
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
@@ -23,9 +25,41 @@ namespace GetBooksProject.Controls
             _condition.SelectedIndex = 0;
         }
 
+        private void ShowResult(List<ProductBook> books)
+        {
+            _slabDisplay.Controls.Clear();
+
+            if (books.Count != 0)
+            {
+                foreach (ProductBook book in books)
+                {
+                    _slabDisplay.Controls.Add(new BookSlab(book, _bookDisplay));
+                }
+            }
+            else
+            {
+                Form1.SetMessage("По вашему запросу ничего не найдено");
+            }
+        }
+
         private void findOnLabirint(object sender, EventArgs e)
         {
-            Form1.SetMessage("Labirint");
+            LabirintParser parser = new LabirintParser();
+
+            string request = _request.Text;
+
+            if (request != string.Empty)
+            {
+                try
+                {
+                    List<ProductBook> books = parser.GetBooks(request);
+                    ShowResult(books);
+                }
+                catch (Exception ex)
+                {
+                    Form1.SetMessage(ex.Message);
+                }
+            }
         }
     }
 }
